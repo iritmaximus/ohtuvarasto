727f755a8627b0ccc9d7f4addcfb0f4576522815
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-11-09 16:31:27 +0200

    refactored code that tests pass

--- src/index.py
+++ src/index.py
@@ -1,27 +1,21 @@
 from varasto import Varasto
 
 
+# näitä funktioita ei ole mielekästä paloitella enää pienemmäksi,
+# näin tekeminen vain monimutkaistaisi aivan turhaan tiedostoa.
+# koko index.py tarkoitus tuntuu olevan vain tulostella kovakoodatuilla
+# tiedoilla sovellusta, eikä siksi mielestäni pitäisi joutua refaktoroimaan
+# ainakaan max-statements rajan takia
+
+# jo nyt tulosteiden järjestys on hieman eri, ilman sitä refaktorointi olisi
+# hyvin turhaa ja aivan liian monimutkaista suhteessa siihen, mitä koodi tekee
 def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
+    mehu_varasto()
+    olut_varasto()
+    huono_varasto()
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
 
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-
-    print("Mehu setterit:")
-    print("Lisätään 50.7")
-    mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14")
-    mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
-
+def huono_varasto():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,21 +25,27 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.lisaa_varastoon(1000.0)")
-    olutta.lisaa_varastoon(1000.0)
-    print(f"Olutvarasto: {olutta}")
+
+# pylint: disable-next=too-many-statements
+def mehu_varasto():
+    mehua = Varasto(100.0)
+
+    print("Luonnin jälkeen:")
+    print(f"Mehuvarasto: {mehua}")
+
+    print("Mehu setterit:")
+    print("Lisätään 50.7")
+    mehua.lisaa_varastoon(50.7)
+    print(f"Mehuvarasto: {mehua}")
+
+    print("Otetaan 3.14")
+    mehua.ota_varastosta(3.14)
+    print(f"Mehuvarasto: {mehua}")
 
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
-    saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
 
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
@@ -53,6 +53,28 @@ def main():
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+# pylint: disable-next=too-many-statements
+def olut_varasto():
+    olutta = Varasto(100.0, 20.2)
+
+    print("Luonnin jälkeen:")
+    print(f"Olutvarasto: {olutta}")
+
+    print("Olut getterit:")
+    print(f"saldo = {olutta.saldo}")
+    print(f"tilavuus = {olutta.tilavuus}")
+    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
+
+    print(f"Olutvarasto: {olutta}")
+    print("olutta.lisaa_varastoon(1000.0)")
+    olutta.lisaa_varastoon(1000.0)
+    print(f"Olutvarasto: {olutta}")
+
+    print(f"Olutvarasto: {olutta}")
+    print("olutta.ota_varastosta(1000.0)")
+    saatiin = olutta.ota_varastosta(1000.0)
+    print(f"saatiin {saatiin}")
+    print(f"Olutvarasto: {olutta}")
 
 if __name__ == "__main__":
     main()
--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -28,18 +29,18 @@ class Varasto:
         else:
             self.saldo = self.tilavuus
 
-    def ota_varastosta(self, x):
-        if x < 0:
+    def ota_varastosta(self, maara):
+        if maara < 0:
             return 0.0
-        if x > self.saldo:
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
             return kaikki_mita_voidaan
 
-        self.saldo = self.saldo - x
+        self.saldo = self.saldo - maara
 
-        return x
+        return maara
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
